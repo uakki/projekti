5788bc46d1f66668bb9af63b0ac027e498de9a5c
Aki Hyvonen <aki1.hyvonen@gmail.com>
2022-03-10 18:17:07 +0200

    asd

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,11 @@
 <?php
 
-$serverName = "local";
+$serverName = "127.0.0.1";
 $dBUserName = "azure";
 $dBPassword = "6#vWHD_$";
 $dBName = "project";
 
-$conn = mysqli_connect($serverName, $dBUserName, $dBPassword, $dBName);
+$conn = mysqli_connect($serverName, $dBUserName, $dBPassword, $dBName, 50690);
 
 if (!$conn){
   die("Connection failed: " . mysqli_connect_error());
